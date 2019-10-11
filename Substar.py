4af25d8997439a9669063be764172262c4766018
Amy (lydocia) <34206847+lydocia@users.noreply.github.com>
2019-10-11 09:48:27 +0200

    Initial commit.

---
+++ Substar.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+###more import stuff goes here
+
+### invite Substar: https://discordapp.com/oauth2/authorize?client_id=CLIENTID&scope=bot&permissions=8 ###
+
+TOKEN = 'TOKENGOESHERE'
+
+description = '''Substarters' bot Substar.'''
+bot = commands.Bot(command_prefix='!', description=description)
+
+@bot.event
+async def on_ready():
+    print(bot.user.name + ' is running.')
+    
+# Add stuff here
+
+bot.run(TOKEN)
