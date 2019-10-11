e5a02b188ea5217e5c12f7b52b12ad952dbf2f5d
Amy (lydocia) <34206847+lydocia@users.noreply.github.com>
2019-10-11 11:53:42 +0200

    Manual push

--- Substar.py
+++ Substar.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
+import tokens
+import strings
 ###more import stuff goes here
 
-### invite Substar: https://discordapp.com/oauth2/authorize?client_id=CLIENTID&scope=bot&permissions=8 ###
-
-TOKEN = 'TOKENGOESHERE'
+### invite Substar: https://discordapp.com/oauth2/authorize?client_id=632129851704475648&scope=bot&permissions=8 ###
 
 description = '''Substarters' bot Substar.'''
 bot = commands.Bot(command_prefix='!', description=description)
@@ -15,4 +15,8 @@ async def on_ready():
     
 # Add stuff here
 
-bot.run(TOKEN)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hi there! I'm Substar!")
+
+bot.run(tokens.Substar)
