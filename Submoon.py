4fcc6b3d4e2e62d4c5bf1c87e85fcd581a5f12b9
Amy (lydocia) <34206847+lydocia@users.noreply.github.com>
2019-10-11 11:49:50 +0200

    Manual push

--- Submoon.py
+++ Submoon.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
+import tokens
+import strings
 ###more import stuff goes here
 
-### invite Substar: https://discordapp.com/oauth2/authorize?client_id=CLIENTID&scope=bot&permissions=8 ###
-
-TOKEN = 'TOKENGOESHERE'
+### invite Submoon: https://discordapp.com/oauth2/authorize?client_id=632130020495589376&scope=bot&permissions=8 ###
 
 description = '''Substarters' bot Submoon.'''
 bot = commands.Bot(command_prefix='!', description=description)
@@ -15,4 +15,5 @@ async def on_ready():
     
 # Add stuff here
 
-bot.run(TOKEN)
+bot.run(tokens.Submoon)
+
